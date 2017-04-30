70b45ea45586b6c5818743f5b3f2bd4b0f4cf584
dylanito2 <dylanito2@gmail.com>
2017-04-30 10:19:15 -0400

    Done.

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,18 @@
+require 'byebug'
 def find_closing_parenthesis(sentence, open_position)
-
+  i = open_position + 1
+  o = 0
+  while (i < sentence.length)
+    if sentence[i] == '('
+      o += 1
+    end
+    if sentence[i] == ')'
+      if o == 0
+        return i
+      else
+        o -= 1
+      end
+    end
+    i += 1
+  end
 end
